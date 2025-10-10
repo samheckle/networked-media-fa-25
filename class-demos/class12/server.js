d05af25876b09c72a506dbe5b7560e831cd955ea
samheckle <samheckle@yahoo.com>
2025-10-10 11:59:22 -0400

    added class 12

---
+++ class-demos/class12/server.js
@@ -0,0 +1,53 @@
+// importing the express library
+// always put imports at the top
+const express = require('express')
+
+// create an instance of the express app
+// just like we do with let date = new Date()
+const app = express()
+
+// middleware to set up the public folder to serve basic html files
+app.use(express.static('htmls'))
+
+// a global variable array to store all of the message that come in from our client
+let messages = []
+
+// routes always go before the app.listen
+// request: data that comes from the user / client
+// response: data that goes back to the user/client
+app.get('/', (request, response)=>{
+    response.send('server is working')
+})
+
+app.get('/submit', (request, response)=>{
+    // query is everything that comes after the ? in the url
+    // ?guest=sam&message=hi+this+is+a+cool+site%21&submitbtn=write
+    console.log(request.query.guest)
+
+    let guest = request.query.guest
+
+    // object that holds the data that comes in from ONE form request
+    let messageData = {
+        username: request.query.guest,
+        text: request.query.message
+    }
+
+    messages.push(messageData)
+
+    response.send('thank you for writing a message, ' + guest) 
+})
+
+// new route that goes after the submit route
+app.get('/all-messages', (request, response)=>{
+    let allMessages = ''
+    for(let m of messages){
+        allMessages += m.username + " says " + m.text + "<br/>"
+    }
+    response.send(allMessages)
+})
+
+// sets up our server
+// this should always go at the end of the file
+app.listen(5001, ()=>{
+    console.log('server started')
+})
